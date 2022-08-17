c8a795d6d0efc52fc85c27c215ceb1ad2776727f
Gabriel Santo <gvgesanto2@gmail.com>
2022-08-17 18:35:45 -0300

    feat: Add queries to answer the assingment questions

--- queries.sql
+++ queries.sql
@@ -25,3 +25,33 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 -- 8- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+-- Project 2:
+
+-- 1- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- 2- How many animals have never tried to escape?
+SELECT COUNT(*) FROM animals
+WHERE escape_attempts = 0;
+
+-- 3- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- 4- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) as total_escapes 
+FROM animals
+GROUP BY neutered 
+ORDER BY SUM(escape_attempts) DESC;
+
+-- 5- What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg) as min_weight, MAX(weight_kg) as max_weight
+FROM animals
+GROUP BY species;
+
+-- 6- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) as average_escape_attempts
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+
